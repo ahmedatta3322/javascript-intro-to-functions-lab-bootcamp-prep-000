22ee05ac28ff9a477b65aa0e2de2b882a8e7c233
ahmed atta <ahmedatta3322@gmail.com>
2018-08-13 15:15:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if(string.toUpperCase() === string){
-    
+    return "I can't hear you!"
+  }
+  else if (string.toLowerCase() === string){
+    return "I can't hear you!"
+  }
   }
   
 }
